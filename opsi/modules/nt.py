59cc0f30cbeb5dfcb34698cb92b0d9d011541bf1
RJ Richards <thegreenbomb3999@gmail.com>
2019-09-25 15:54:24 -0400

    Add features to NT module

--- opsi/modules/nt.py
+++ opsi/modules/nt.py
@@ -1,19 +1,26 @@
-import time
 from dataclasses import dataclass
 
 from networktables import NetworkTables
 
 from opsi.manager.manager_schema import Function
 
-
 class InitializeNT(Function):
     @dataclass
     class Settings:
         teamnum: int
+        client: bool
+        staticIP: bool
 
     def run(self, inputs):
-        mDNS = f"roboRIO-{self.settings.teamnum}-FRC.local"
-        NetworkTables.initialize(server=mDNS)
+        if not self.settings.client:
+            NetworkTables.initialize()
+        else:
+            static = f"10.{self.settings.teamnum[:2]}.{self.settings.teamnum[:-2]}.2"
+            mDNS = f"roboRIO-{self.settings.teamnum}-FRC.local"
+            if self.settings.staticIP:
+                NetworkTables.initialize(server=static)
+            else:
+                NetworkTables.initialize(server=mDNS)
 
 
 class PutInteger(Function):
@@ -22,11 +29,47 @@ class PutInteger(Function):
 
     @dataclass
     class Settings:
-        name: str
+        key: str
 
     @dataclass
     class Inputs:
         val: int
 
     def run(self, inputs):
-        self.table.putInt(self.settings.name, inputs.val)
+        self.table.putNumber(self.settings.key, inputs.val)
+
+
+class PutString(Function):
+    def on_start(self):
+        self.table = NetworkTables.getTable("SmartDashboard")
+
+    @dataclass
+    class Settings:
+        key: str
+
+    @dataclass
+    class Inputs:
+        val: int
+
+    def run(self, inputs):
+        self.table.putString(self.settings.key, inputs.val)
+
+
+class PutCoordinate(Function):
+    def on_start(self):
+        self.table = NetworkTables.getTable("SmartDashboard")
+
+    @dataclass
+    class Settings:
+        key: str
+
+    @dataclass
+    class Inputs:
+        val: ()
+
+    def run(self, inputs):
+        xkey = self.settings.key + "-x"
+        ykey = self.settings.key + "-y"
+        x, y = inputs.val[0], inputs.val[1]
+        self.table.putNumber(xkey, x)
+        self.table.putNumber(ykey, y)
