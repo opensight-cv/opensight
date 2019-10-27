06f5d34006eea5f4279dffbd1c5e271bfc58eaee
dzil123 <5725958+dzil123@users.noreply.github.com>
2019-10-26 23:08:12 -0700

    F/B: Fix Stuff

--- opsi/__init__.py
+++ opsi/__init__.py
@@ -5,6 +5,9 @@ from opsi.util.path import join
 # silence fastapi warning about email-validator
 logging.getLogger("fastapi").setLevel(logging.ERROR)
 
+# silence mutlipart excessive debug output
+logging.getLogger("multipart").setLevel(logging.ERROR)
+
 __version__ = "master"
 
 try:
