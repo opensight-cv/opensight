046f6920f1c666492976db2b123c31ed7aa8502f
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2020-01-09 01:59:24 -0500

    Add H264 to CameraServer
    The existing function was redone as a class, then H264CameraServer was made in its image. The CameraServer function is essentially a wrapper around one of the two now, chose on on_start.
    
    H264CameraServer sacrifices the first frame as a way to determine image dimensions and is locked to 30 FPS for safety. (I have no idea what happens if you make target FPS higher than reality.)

--- opsi/modules/videoio/__init__.py
+++ opsi/modules/videoio/__init__.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from typing import Tuple
+
+import engine
 
 from opsi.manager.manager_schema import Function
 from opsi.util.cv import Mat, MatBW
@@ -9,7 +12,6 @@ from .input import controls, create_capture, get_modes, parse_camstring
 
 __package__ = "opsi.videoio"
 __version__ = "0.123"
-
 
 UndupeInstance = Unduplicator()
 HookInstance = CamHook()
@@ -63,11 +65,14 @@ class CameraServer(Function):
         return settings
 
     def on_start(self):
-        self.src = CameraSource()
-        HookInstance.register(self)
+        if self.settings.backend == "MjpegCameraServer":
+            self.src = MjpegCameraServer()
+        elif self.settings.backend == "H264CameraServer":
+            self.src = H264CameraServer()
 
     @dataclass
     class Settings:
+        backend: ("MjpegCameraServer", "H264CameraServer")
         name: str = "camera"
 
     @dataclass
@@ -75,17 +80,52 @@ class CameraServer(Function):
         img: Mat
 
     def run(self, inputs):
-        if isinstance(inputs.img, MatBW):
-            self.src.img = inputs.img.mat
-        else:
-            self.src.img = inputs.img
+        self.src.run(inputs)
         return self.Outputs()
 
     def dispose(self):
-        self.src.shutdown()
-        HookInstance.unregister(self)
+        self.src.dispose()
 
     # Returns a unique string for each CameraServer instance
     @property
     def id(self):
         return self.settings.name
+
+
+class MjpegCameraServer:
+    def __init__(self):
+        self.src = CameraSource()
+        HookInstance.register(self)
+
+    def run(self, inputs):
+        if isinstance(inputs.img, MatBW):
+            self.src.img = inputs.img.mat
+        else:
+            self.src.img = inputs.img
+
+    def dispose(self):
+        self.src.shutdown()
+        HookInstance.unregister(self)
+
+
+class H264CameraServer:
+    def __init__(self):
+        self.engine: engine.GStreamerEngineWriter = None
+
+    def run(self, inputs: "CameraServer.Inputs"):
+        if self.engine is None:
+            # we need to set up engine
+            size: Tuple[int, int, int] = (*inputs.img.img.size, 30)
+            self.engine = engine.GStreamerEngineWriter(
+                video_size=size, repeat_frames=True
+            )
+            return
+        else:
+            if isinstance(inputs.img, MatBW):
+                img = inputs.img.mat
+            else:
+                img = inputs.img
+            self.engine.write_frame(img)
+
+    def dispose(self):
+        self.engine.end()
