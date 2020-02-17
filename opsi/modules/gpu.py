8d299ec153237ef145acc1433501db718817e67b
Jack LeFevre <jackflef@gmail.com>
2020-02-16 19:41:48 -0500

    Made GPU Threshold stop segfault-ing

--- opsi/modules/gpu.py
+++ opsi/modules/gpu.py
@@ -15,7 +15,7 @@ DIM_BLOCK = 32
 
 class ThresholdGPU(Function):
     def on_start(self):
-        self.gpu_thresh = CudaThresholdWrapper()
+        self.gpu_thresh = CudaThresholdWrapper(*self.lower_upper_from_settings())
 
     @dataclass
     class Settings:
@@ -31,16 +31,51 @@ class ThresholdGPU(Function):
     class Outputs:
         img: MatBW
 
+    def lower_upper_from_settings(self):
+        lower = (self.settings.hue[0], self.settings.sat[0], self.settings.val[0])
+        upper = (self.settings.hue[1], self.settings.sat[1], self.settings.val[1])
+        return lower, upper
+
     def run(self, inputs):
         if inputs.img is None:  # Do not write None values to NT
             return self.Outputs()
 
-        lower = (self.settings.hue[0], self.settings.sat[0], self.settings.val[0])
-        upper = (self.settings.hue[1], self.settings.sat[1], self.settings.val[1])
+        lower, upper = self.lower_upper_from_settings()
+        self.gpu_thresh.update_kernel(lower, upper)
 
-        processed = self.gpu_thresh.apply(inputs.img.img, lower, upper)
+        processed = self.gpu_thresh.apply(inputs.img.img)
 
         return self.Outputs(MatBW(processed))
+
+
+class CudaThresholdWrapper:
+    def __init__(self, lower, upper):
+        self.lower = lower
+        self.upper = upper
+        self.cuda_kernel = self.compile_kernel(lower, upper)
+
+    def update_kernel(self, lower, upper):
+        if lower != self.lower or upper != self.upper:
+            self.lower = lower
+            self.upper = upper
+            self.cuda_kernel = self.compile_kernel(lower, upper)
+
+    def compile_kernel(self, lower, upper):
+        return cp.ElementwiseKernel(
+            'uint8 r, uint8 g, uint8 b',
+            'uint8 out',
+            f'if(r > {lower[0]} && r < {upper[0]} && g > {lower[1]} && g < {upper[1]} && b > {lower[2]} && b < {upper[2]}) {{ out = 255; }} else {{ out = 0; }}',
+            'threshold'
+        )
+
+    def apply(self, source_array):
+        red_channel = cp.asarray(source_array[:, :, 0])  # TODO Make work for greyscale images
+        green_channel = cp.asarray(source_array[:, :, 1])
+        blue_channel = cp.asarray(source_array[:, :, 2])
+
+        result_array = self.cuda_kernel(red_channel, green_channel, blue_channel)
+
+        return cp.asnumpy(result_array)
 
 
 class BlurGPU(Function):
@@ -68,40 +103,6 @@ class BlurGPU(Function):
         processed = self.gpu_blur.apply(inputs.img.img)
 
         return self.Outputs(Mat(processed))
-
-
-class CudaThresholdWrapper:
-    def __init__(self):
-        self.cuda_module = cp.RawModule(code=open('./opsi_cuda/threshold.cu').read())
-        self.apply_filter = self.cuda_module.get_function("applyFilter")
-
-    def apply(self, source_array, lower, upper):
-        red_channel = cp.asarray(source_array[:, :, 0])  # TODO Make work for greyscale images
-        green_channel = cp.asarray(source_array[:, :, 1])
-        blue_channel = cp.asarray(source_array[:, :, 2])
-
-        height, width = source_array.shape[:2]
-
-        result_array = cp.empty((height, width, 1), dtype=cp.uint8)
-
-        dim_grid_x = math.ceil(width / DIM_BLOCK)
-        dim_grid_y = math.ceil(height / DIM_BLOCK)
-
-        self.apply_filter(
-                (dim_grid_x, dim_grid_y),
-                (DIM_BLOCK, DIM_BLOCK),
-                (
-                    red_channel, green_channel, blue_channel,
-                    result_array,
-                    cp.uint32(width),
-                    cp.uint32(height),
-                    lower[0], upper[0],
-                    lower[1], upper[1],
-                    lower[2], upper[2]
-                )
-            )
-
-        return cp.asnumpy(result_array)
 
 
 class CudaBlurWrapper:
