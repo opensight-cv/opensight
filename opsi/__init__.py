4b9c7c336138c9e2221a23606f1b1ece85fe92eb
132ikl <132@ikl.sh>
2020-04-14 17:59:24 -0400

    Show real branch as version

--- opsi/__init__.py
+++ opsi/__init__.py
@@ -1,6 +1,5 @@
 import logging
-
-from opsi.util.path import join
+from pathlib import Path
 
 # silence fastapi warning about email-validator
 logging.getLogger("fastapi").setLevel(logging.ERROR)
@@ -8,10 +7,20 @@ logging.getLogger("fastapi").setLevel(logging.ERROR)
 # silence mutlipart excessive debug output
 logging.getLogger("multipart").setLevel(logging.ERROR)
 
-__version__ = "master"
+__version__ = None
 
 try:
-    with open(join(__file__, "version.py")) as f:
+    with open(Path(__file__) / "version.py") as f:
         __version__ = f.read().strip()
 except OSError:
     pass
+
+if not __version__:
+    try:
+        with open(Path(__file__).parent.parent / ".git/HEAD") as f:
+            __version__ = "/".join(f.read().split("/")[2:]).strip()
+    except (OSError, IndexError) as e:
+        pass
+
+if not __version__:
+    __version__ = "unknown"
