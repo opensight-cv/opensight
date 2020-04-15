1afbc82a998cafb51a114ed94f7820d043b91631
dzil123 <5725958+dzil123@users.noreply.github.com>
2020-04-14 17:09:08 -0700

    M: mjpeg Params validation rewrite (untested)

--- opsi/modules/videoio/mjpeg.py
+++ opsi/modules/videoio/mjpeg.py
@@ -1,9 +1,9 @@
 import asyncio
 import logging
-from dataclasses import dataclass
 from datetime import datetime, timedelta
+from typing import Tuple
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, ValidationError, validator
 
 from opsi.util.asgi import ASGIStreamer
 from opsi.util.cv import Point
@@ -12,39 +12,92 @@ LOGGER = logging.getLogger(__name__)
 # logging.getLogger("asyncio").setLevel(logging.ERROR)
 
 
+class Params(BaseModel):
+    @validator("compression", "fps", pre=True)
+    def parse_float_str(cls, v):
+        # typically, with a field of type int,
+        #    30    -> 30
+        #   "30"   -> 30
+        #    30.9  -> 30
+        #   "30.9" -> error
+
+        try:
+            return int(v)
+        except ValueError:
+            return int(float(v.strip("'\"")))
+
+    @validator("resolution", pre=True)
+    def parse_resolution(cls, v):
+        # parse widthXheight str
+
+        if v is None:
+            return v
+
+        try:
+            # split only, pydantic will convert to int
+            return v.lower().split("x")
+        except AttributeError:
+            log_bad_resolution(v)
+            return None
+
+    @validator("resolution")
+    def convert_resolution(cls, v):
+        # convert Tuple[int, int] to Point
+
+        if v is None or isinstance(v, Point):
+            return v
+
+        try:
+            return Point(int(v[0]), int(v[1]))
+        except (ValueError, TypeError):
+            log_bad_resolution(v)
+            return None
+
+    @classmethod
+    def create(cls, query=None):
+        if query is None or not len(query):
+            return cls()
+
+        try:
+            return cls.parse_obj(query)
+        except ValidationError as e:
+            # remove the broken keys, keep the working ones
+            # {"fps": "foobar", "compression": 10} -> Params(compression=10, fps=default, ...)
+
+            query = dict(query)  # original query is immutable
+
+            for error in e.errors():
+                try:
+                    del query[error["loc"][0]]
+                except KeyError:  # multiple errors in same key
+                    pass
+
+            try:
+                return cls.parse_obj(query)
+            except ValidationError:
+                return cls()
+
+    compression: int = Field(30, ge=0, le=100)
+    fps: int = Field(30, gt=0)
+    resolution: Tuple[int, int] = None  # actually type: Point
+
+
 # An ASGI application that streams mjpeg from a jpg iterable
 class MjpegResponse:
-    HEADERS = ASGIApplication._encode_bytes("Content-Type: image/jpeg\r\n\r\n")
+    HEADERS = ASGIStreamer._encode_bytes("Content-Type: image/jpeg\r\n\r\n")
 
     def __init__(self, request, camserv):
         self.request = request
         self.camserv = camserv
 
-    class Params(BaseModel):
-        compression: int = 30
-        fps: int = 30
-        resolution: Point = None
-
-    def get_params(self):
-        query = dict(self.request.query_params)
-
-        # parse resolution (in format XXxYY)
-        res = query.get("resolution")
-        if res:
-            try:
-                r = res.split("x")
-                query["resolution"] = Point(int(r[0]), int(r[1]))
-            except (ValueError, AttributeError):
-                LOGGER.error("Invalid resolution: %s", res)
-                query["resolution"] = None
-
-        params = self.Params.parse_obj(query)
-        return params
-
     async def __call__(self, scope, receive, send):
         # call app.send(self.HEADERS + frame) to send frame
         sink = self.camserv.src.src
-        params = self.get_params()
+        query = dict(self.request.query_params)
+        params = Params.create(query)
+
+        LOGGER.info("Parsed params: %r -> %r", query, params)
+
         async with ASGIStreamer(receive, send) as app:
             while True:
                 if app.end or sink.end:
