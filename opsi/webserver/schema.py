70102924c8af93fc77098e0335fe9d41951a38f6
Quy Nguyen <quy.d.x.nguyen@gmail.com>
2020-04-27 22:37:02 -0500

    Comply with style

--- opsi/webserver/schema.py
+++ opsi/webserver/schema.py
@@ -27,7 +27,7 @@ class NodeN(BaseModel):
     type: str
     id: str
     settings: Dict[str, Any] = {}
-    frontendOpts: Dict[str, Any] = {}
+    extras: Dict[str, Any] = {}
     if LINKS_INSTEAD_OF_INPUTS:
         inputs: Dict[str, Optional[LinkN]] = {}
     else:
@@ -36,7 +36,7 @@ class NodeN(BaseModel):
 
 class NodeTreeN(BaseModel):
     nodes: List[NodeN] = []
-    frontendOpts: Dict[str, Any] = {}
+    extras: Dict[str, Any] = {}
 
 
 # --------------------------------
