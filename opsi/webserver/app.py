debaad06d6fb06f9b5abcaacbdfc896b8968af7a
132ikl <132@ikl.sh>
2020-04-29 05:57:53 -0400

    W: Fix Content-Length GZip issue by swapping Cache and GZip middlewares

--- opsi/webserver/app.py
+++ opsi/webserver/app.py
@@ -33,8 +33,8 @@ class WebServer:
 
         self.app.mount(
             "/",
-            GZipMiddleware(
-                CacheControlMiddleware(
+            CacheControlMiddleware(
+                GZipMiddleware(
                     StaticFiles(directory=join(frontend, "dist"), html=True)
                 ),
             ),
